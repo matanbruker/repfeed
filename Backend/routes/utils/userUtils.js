ab5192f75bc097969801a1eae45be8df9bbe5ddd
shamaid <shamaid@post.bgu.ac.il>
2020-12-17 15:23:29 +0200

    commit

--- Backend/routes/utils/userUtils.js
+++ Backend/routes/utils/userUtils.js
@@ -4,6 +4,9 @@ const axios = require("axios");
 const users_api_url = "https://api.twitter.com/1.1/followers";
 const token =process.env.twitter_apiKey;
 
+const DBUtils = require("./DBUtils");
+
+
 async function followers() {
 //   let followers = await axios.get(`${users_api_url}
 // /list.json?cursor=-1&screen_name=realDonaldTrump&skip_status=true&include_user_entities=false`);
@@ -25,5 +28,33 @@ return users_data
 //console.log(followers[data][users]);
 
 }
+
+
+//jast a try - not working function (showing itay)
+async function getTweetByScore(req) {
+  //   let followers = await axios.get(`${users_api_url}
+  // /list.json?cursor=-1&screen_name=realDonaldTrump&skip_status=true&include_user_entities=false`);
+    
+  score = req.param
+
+  // go to DB
+  users = DBUtils.getUsersByScore(score)
+
+  ////////////////////////////////////////////////////////////////////
+  let followers = await axios.get(`${users_api_url}/list.json?cursor=-1&screen_name=realDonaldTrump&skip_status=true&include_user_entities=false`, {
+   headers: {
+     Authorization: 'Bearer ' + token //the token is a variable which holds the token
+   }
+  })
+  
+  //console.log(followers);
+  //console.log(followers.data);
+  //console.log(followers.data.users);
+  
+  const users_data = followers.data
+  return users_data
+  //console.log(followers[data][users]);
+  
+  }
 
 exports.followers = followers;

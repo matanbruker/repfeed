e1f4f04c4f2811b86a9e66bdbe463a21bf531b0d
shamaid <shamaid@post.bgu.ac.il>
2020-12-30 10:38:04 +0200

    indent repfeed

--- Backend/routes/repfeed.js
+++ Backend/routes/repfeed.js
@@ -4,68 +4,49 @@ var router = express.Router();
 const repfeedUtils = require("./utils/repfeedUtils");
 const DBUtils = require("./utils/DBUtils");
 
-
-
-
 router.get("/repFeed", (req, res) => {
+  if (req.moreTweets == false) {
+    if (req.score != null) {
+      repfeedUtils
+        .buildRepFeedByBar(req.score)
+        .then((tweets_text) => res.send(tweets_text))
+        .catch((error) => {
+          console.log(error);
+          res.sendStatus(404);
+        });
+    } else {
+      repfeedUtils
+        .resetRepFeed()
+        .then((tweets_text) => res.send(tweets_text))
+        .catch((error) => {
+          console.log(error);
+          res.sendStatus(404);
+        });
+    }
+  } else {
+    // if(req.score != null){
+    repfeedUtils
+      .showTweets()
+      .then((tweets_text) => res.send(tweets_text))
+      .catch((error) => {
+        console.log(error);
+        res.sendStatus(404);
+      });
 
-  if(req.moreTweets == false){
-  if(req.score != null){
-    repfeedUtils
-    .buildRepFeedByBar(req.score)
-    .then((tweets_text) => res.send(tweets_text))
-    .catch((error) => {
-        console.log(error);
-        res.sendStatus(404);
-    });
-  
-  }else{
-    repfeedUtils
-    .resetRepFeed()
-    .then((tweets_text) => res.send(tweets_text))
-    .catch((error) => {
-        console.log(error);
-        res.sendStatus(404);
-    });
-  
+    // }
   }
-}else{
-  // if(req.score != null){
-    repfeedUtils
-    .showTweets()
-    .then((tweets_text) => res.send(tweets_text))
-    .catch((error) => {
-        console.log(error);
-        res.sendStatus(404);
-    });
-  
-  // }
-}
-  
-  );
-}
+});
 
-  // router.get("/", (req, res) => {
+// router.get("/", (req, res) => {
 
-  //   repfeedUtils
-  //   .getTweetByScore(req)
-  //   .then((users_data) => res.send(users_data))
-  //   .catch((error) => {
-  //       console.log(error);
-  //       res.sendStatus(404);
-  //   });
-  
-  // });
+//   repfeedUtils
+//   .getTweetByScore(req)
+//   .then((users_data) => res.send(users_data))
+//   .catch((error) => {
+//       console.log(error);
+//       res.sendStatus(404);
+//   });
 
-
-
-
-
-
-
-
-
-
-
+// });
 
 module.exports = router;
